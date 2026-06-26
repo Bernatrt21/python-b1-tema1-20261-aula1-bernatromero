6756d39230e9ead344d9ba4b33a43318de3204fa
Bernatrt21 <bernatrt21@gmail.com>
2026-06-26 09:27:33 +0200

    Update ej1b4.py

--- 1b/ej1b4.py
+++ 1b/ej1b4.py
@@ -66,8 +66,11 @@ import numpy as np
 
 def results(list_numbers):
     # Write here your code
-    pass
-
+    mitjana = np.mean(list_numbers)
+    estandart = np.std(list_numbers)
+    print("Mitjana: ", {round(mitjana, 2)})
+    print("Deviacio estandart: ", {round(estandart, 2)})
+    return mitjana, estandart 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
